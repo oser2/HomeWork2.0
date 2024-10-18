ebca9341e8be300c73b1e0b379b33c1119815490
o_ser <ostapius.2009@gmail.com>
2024-10-18 19:31:24 +0300

    add

--- script.js
+++ script.js
@@ -41,4 +41,38 @@ let res = arr.every((item, index, array) => item % 2 == 0)
 console.log(res)
 methodsRes[5].textContent = res
 
-// метод 
+// метод some
+arr = [5, -3, -4, 0, 1]
+arr = arr.some(item => item > 0)
+console.log(arr)
+methodsRes[6].textContent = arr
+
+//метод fill
+arr = [3, 2, 10, -3, 0, 7, 'k', 4]
+arr = arr.fill('rep', 2, 6)
+console.log(arr)
+methodsRes[7].textContent = arr
+
+//indexOf
+arr = [3, 2, 10, 5, 0, 7, 5, 4]
+arr = arr.indexOf(5, 4)
+console.log(arr)
+methodsRes[8].textContent = arr
+
+//unshift
+arr = [3, 2, 10]
+arr.unshift(4, 5)
+console.log(arr)
+methodsRes[9].textContent = arr
+
+//pop
+arr = [3, 2, 10, 'good']
+arr.pop()
+console.log(arr)
+methodsRes[10].textContent = arr
+
+//pop
+arr = [3, 2, 10, 'bet']
+arr.shift()
+console.log(arr)
+methodsRes[11].textContent = arr
